1cf714b529340aecc3474b68355b1a210d4e7656
Petri Ojala <petri.o.ojala@tietoevry.com>
2024-07-12 12:45:12 +0300

    private endpoint for terraform state storage

--- sas-esp-terraform.tf
+++ sas-esp-terraform.tf
@@ -20,6 +20,25 @@ module "sas_esp_terraform_state_storage" {
   ]
 }
 
+module "sas_esp_terraform_state_storage_private_endpoint" {
+  source = "./azure-network"
+
+  reference = {
+    azure_resource_group   = module.sas_esp_resource_group.azure_resource_group
+    azure_private_dns_zone = module.sas_esp_dns.azure_private_dns_zone
+    azure_subnet           = module.sas_esp_network.azure_subnet
+    azure_resource_id = merge(
+      {
+        for resource_id, resource in module.sas_esp_terraform_state_storage.azure_storage_account : "storage-account-${resource_id}" => resource.id
+      }
+    )
+  }
+
+  common  = var.sas_esp_common
+  network = lookup(var.sas_esp_tietoevry_azure_terraform, "storage_private_endpoint", {})
+}
+
+
 module "sas_esp_terraform_keyvault" {
   source = "./azure-keyvault"
 
